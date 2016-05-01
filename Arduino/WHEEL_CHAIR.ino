271c16e31fcf05e6546c194cb59eabc921bee2ec
Vishnu Anand <VishnuAnand@users.noreply.github.com>
2016-05-01 17:21:05 +0530

    Receiving the Value from Serial Port
    The Variable "val" is declared to receive the value Received from the Bluetooth Module. This will be an Integer Value from 1 to 5.
    Then Switching action using Switch case, then sending signals using digitalWrite to each ports.

--- Arduino/WHEEL_CHAIR.ino
+++ Arduino/WHEEL_CHAIR.ino
@@ -16,5 +16,64 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(Serial.available())  
+  {  
+   int val = Serial.read();
+   delay(20);  
+   switch (val)
+    {
+      case 1: 
+        digitalWrite(LM1, 0);  
+        digitalWrite(RM1, 0); 
+        digitalWrite(LM2, 1);  
+        digitalWrite(RM2, 1);  
+        delay(200);
+        digitalWrite(LM1, LOW);  
+        digitalWrite(RM1, LOW); 
+        digitalWrite(LM2, LOW);  
+        digitalWrite(RM2, LOW);  
+        Serial.println("FORWARD");
+        break;
+      case 2 : 
+        digitalWrite(LM1,HIGH);
+        digitalWrite(LM2,LOW);
+        digitalWrite(RM1,1);
+        digitalWrite(RM2,0);    
+        Serial.println("BACKWARD");
+        delay(200);
+        digitalWrite(LM1, LOW);  
+        digitalWrite(RM1, LOW); 
+        digitalWrite(LM2, LOW);  
+        digitalWrite(RM2, LOW); 
+        break;
+      case 4 :
+        digitalWrite(LM1,1);
+        digitalWrite(LM2,0);
+        digitalWrite(RM1,0);
+        digitalWrite(RM2,1); 
+        delay(200);
+        digitalWrite(LM1, LOW);  
+        digitalWrite(RM1, LOW); 
+        digitalWrite(LM2, LOW);  
+        digitalWrite(RM2, LOW);   
+        Serial.println("LEFT TURN");
+        break;
+      case 3 :
+        digitalWrite(LM1,0);
+        digitalWrite(LM2,1);
+        digitalWrite(RM1,1);
+        digitalWrite(RM2,0);   
+        delay(200);
+        digitalWrite(LM1, LOW);  
+        digitalWrite(RM1, LOW); 
+        digitalWrite(LM2, LOW);  
+        digitalWrite(RM2, LOW);
+        Serial.println("RIGHT TURN");
+        break;
+      case 5 : 
+        digitalWrite(ALM,HIGH);
+        delay(200);
+        digitalWrite(ALM,LOW);   
+      }  
+    }
 }
